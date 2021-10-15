a9f11a743562993f4046fc0ac8e67714d9a2a2ee
Diego Alves <diegoalvesribeiro1993@gmail.com>
2021-10-15 14:44:58 -0300

    primeiro commit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -35,10 +35,9 @@ function Home() {
       <Row gutter={[16, 16]}>
         <Col span={24} md={16}>
           <h2>World</h2>
-         <World values={news?.world} />
+        <World values={news?.world}/>
         </Col>
-        
-        <Col span={24} md={8}>
+        <Col span={24} md={6}>
           <h2>Economy</h2>
           <Economy values={news?.economy} />
         </Col>
